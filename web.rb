aeae02d8c252c9c3da338cc05bf4f8596218b047
Charles Francis <francisc@stripe.com>
2016-04-12 17:43:10 -0400

    Multikey

--- web.rb
+++ web.rb
@@ -4,7 +4,14 @@ require 'dotenv'
 
 Dotenv.load
 
-Stripe.api_key = ENV['STRIPE_TEST_SECRET_KEY']
+api_keys = {
+  'H' => ENV['1'],
+  'S' => ENV['2'],
+  'C' => ENV['3'],
+  'A' => ENV['4'] 
+}
+
+Stripe.api_key = api_keys['H']
 
 get '/' do
   status 200
@@ -12,15 +19,17 @@ get '/' do
 end
 
 post '/charge' do
-
   # Get the credit card details submitted by the form
   token = params[:stripeToken]
+  country = params[:country]
+  Stripe.api_key = api_keys[country][0,1]
 
   # Create the charge on Stripe's servers - this will charge the user's card
   begin
     charge = Stripe::Charge.create(
       :amount => params[:amount], # this number should be in cents
       :currency => "usd",
+      :recurring => "false",
       :card => token,
       :description => "Example Charge"
     )
@@ -31,5 +40,33 @@ post '/charge' do
 
   status 200
   return "Order successfully created"
+
+end
+
+post '/recurring' do
+  # Get the credit card details submitted by the form
+  token = params[:stripeToken]
+  country = params[:country]
+  Stripe.api_key = api_keys[country][0,1]
+  
+  # Create the charge on Stripe's servers - this will charge the user's card
+  begin
+    customer = Stripe::Customer.create(
+      :source => token      
+    )
+    charge = Stripe::Charge.create(
+      :amount => params[:amount], # this number should be in cents
+      :currency => "usd",
+      :source => customer.id,
+      :recurring => "true",
+      :description => "Example Recurring Charge"
+    )
+  rescue Stripe::CardError => e
+    status 402
+    return "Error creating charge."
+  end
+
+  status 200
+  return "Recurring payment successfully created"
 
 end
